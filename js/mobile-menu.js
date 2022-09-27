a7b2e2d40c62e13fe70ef43a31361978e9348b73
olgagaleta <olga.galeta.og@gmail.com>
2022-09-27 13:08:53 +0100

    Адаптує верстку залежно від розміру вікна браузера

---
+++ js/mobile-menu.js
@@ -0,0 +1,16 @@
+// <!-- Скрипт для мобільного меню -->
+
+(() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+    const mobileBtnClose = document.querySelector("[data-menu-close]");
+  
+    menuBtnRef.addEventListener("click", () => {
+      mobileMenuRef.classList.toggle("is-open");
+    });
+  
+    mobileBtnClose.addEventListener("click", () => {
+      mobileMenuRef.classList.toggle("is-open");
+    });
+  })();
+  
